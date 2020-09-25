46e4a42072539038328b94c0c9cfbc0441d11039
Yogesh badatya <yogesh@TBI.local>
2020-09-25 16:53:43 +0530

    changes in app file

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const loading = (
 const TheLayout = React.lazy(() => import('./containers/TheLayout'));
 
 // Pages
-const Login = React.lazy(() => import('./views/pages/login/Login'));
+const Login =  React.lazy(() => import('./views/pages/login/Login'));
 const Register = React.lazy(() => import('./views/pages/register/Register'));
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
